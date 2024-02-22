f2e1734dcbe05698b34793db5cd019feb15be9df
22DP3ASulg <131255102+22DP3ASulg@users.noreply.github.com>
2024-02-22 13:50:15 +0000

    1

---
+++ src/main/java/rvt/Money.java
@@ -0,0 +1,12 @@
+public class Money {
+    
+}
+public boolean equals(Object compared){
+    if (this == compared){
+        return true;
+    }
+    if (!(compared instanceof Money)){
+        return false;
+    }
+    Money
+}
